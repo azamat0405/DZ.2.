f2d7d1a4fb9952d5516e61bf77044d422c8ab511
azamat0405 <azamat040@bk.ru>
2020-02-23 19:27:47 +0300

    ю

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(wind(40 ,50 ));
+
+    }
+
+    public static String wind(int age, int temp) {
+        if (age > 20 && age < 45 && temp > -20 && temp < 30) {
+            return "можно прогуляться";
+
+        } else if (age > 20 && temp > 0 && temp > 30) {
+            return "можно прогуляться";
+
+        } else if (age < 45 && temp < -10 && temp < 25) {
+            return "можно прогуляться";
+
+        } else {
+            return "Оставайтесь дома";
+        }
+
+
+    }
+
+    public static int generateRandomAge() {
+        Random rand = new Random();
+        int rand_1i = rand.nextInt(60) + 1;
+        return rand_1i;
+    }
+}
+
+
