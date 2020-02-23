6f04f0dd406e9590a2f18b521cfbda1d8da82cf3
azamat0405 <azamat040@bk.ru>
2020-02-23 19:38:28 +0300

    рваыр

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(wind(40 ,50 ));
+        System.out.println(wind(35 ,50 ));
 
     }
 
